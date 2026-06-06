a7516afcc5f21a9ac53329e5124c634266ee49c1
Akhil VS <akhilvs47@gmail.com>
2026-06-06 12:32:31 +0530

    I created python basic file

---
+++ python_basics.py
@@ -0,0 +1,9 @@
+name = "Akhil VS"
+college = "Amal College of Advanced Studies, Nilambur"
+age = 20
+branch = "B.Voc Mobile Application"
+
+print("Name:", name)
+print("College:", college)
+print("Age:", age)
+print("Branch:", branch)
